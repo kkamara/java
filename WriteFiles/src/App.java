0c0a106711364bdf40d7cd9a00517cf742b20c83
kkamara <kamaracomputers@gmail.com>
2026-01-30 17:35:08 +0000

    Add project: WriteFiles

---
+++ WriteFiles/src/App.java
@@ -0,0 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // 4 popular options
+
+        // FileWriter = Good for small or medium-sized text files
+        // BufferedWriter = Better performance for large amounts of text
+        // PrintWriter = Best for structured data, like reports or logs
+        // FileOutputStream = Best for binary files (e.g. images, audio files).
+
+        String filePath = "C:\\Users\\doesntexist\\workspace\\test.txt";
+        String textContent = """
+                Some multi-line
+                text content
+                for the file.
+                """;
+
+        try {
+            FileWriter writer = new FileWriter(
+                filePath
+            );
+
+            writer.write(textContent);
+            System.out.println("File has been written.");
+
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not locate file location.");
+        } catch (IOException e) {
+            System.out.println("Could not write file.");
+        }
+    }
+}
